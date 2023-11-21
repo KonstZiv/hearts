657a958e74b5726291affeb4403feff6687d55a0
kos_zivenko <kos.zivenko@gmail.com>
2023-11-21 15:45:48 +0100

    added: first code without variants

---
+++ game.py
@@ -0,0 +1,74 @@
+import random
+from typing import Tuple, List, Sequence, TypeVar, Optional
+
+Card = Tuple[str, str]
+Deck = List[Card]
+
+SUITS = "♠ ♡ ♢ ♣".split()
+RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
+
+Choosable = TypeVar("Choosable", str, Card)
+
+
+def create_deck(shuffle: bool = False) -> Deck:
+    """Create a new deck (52 cards)."""
+
+    deck = [(s, r) for r in RANKS for s in SUITS]
+    if shuffle:
+        random.shuffle(deck)
+    return deck
+
+
+def deal_hands(deck: Deck) -> Tuple[Deck, Deck, Deck, Deck]:
+    """Deal the cards in the deck into four hands."""
+
+    return deck[0::4], deck[1::4], deck[2::4], deck[3::4]
+
+
+def choose(items: Sequence[Choosable]) -> Choosable:
+    """Choose and return a random item."""
+
+    return random.choice(items)
+
+
+def get_player_order(names: Sequence[str], start: Optional[str] = None) -> Sequence[str]:
+    """Rotate player order so that start goes first."""
+    if start is None:
+        start = choose(names)
+    # TODO Check: start in names!
+    start_idx = names.index(start)
+    return names[start_idx:] + names[:start_idx]
+
+
+# def play() -> None:
+#     """Play a 4-player card game."""
+#
+#     deck = create_deck(shuffle=True)
+#     names = "P1 P2 P3 P4".split()
+#     hands = {name: players_deck for name, players_deck in zip(names, deal_hands(deck))}
+#
+#     for name, cards in hands.items():
+#         card_str = " ".join(f"{s}{r}" for (s, r) in cards)
+#         print(f"{name}: {card_str}")
+
+
+def play() -> None:
+    """Play a 4-player card game."""
+    deck = create_deck(shuffle=True)
+    names = "P1 P2 P3 P4".split()
+    hands = {name: players_deck for name, players_deck in zip(names, deal_hands(deck))}
+
+    start_player = choose(names)
+    turn_order = get_player_order(names, start=start_player)
+
+    # Randomly play cards from each player's hand until empty
+    while hands[start_player]:
+        for name in turn_order:
+            card = choose(hands[name])
+            hands[name].remove(card)
+            print(f"{name}: {card[0] + card[1]:<3}  ", end="")
+        print()
+
+
+if __name__ == "__main__":
+    play()
